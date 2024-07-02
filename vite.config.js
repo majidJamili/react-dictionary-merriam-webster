4fcda4a12ffeb5ad4f55a4f73e2a486bcc7764e5
Majid <jamilimajid@gmail.com>
2024-07-02 18:34:35 +1000

    fixed local developemnt

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/react-dictionary-merriam-webster/',
-  build:{
-    rollupOptions:{
-      input:'public/index.html'
-    }
-  },
-  server:{
-    historyApiFallback:true,
-  }
+  // base:'/react-dictionary-merriam-webster/',
+  // build:{
+  //   rollupOptions:{
+  //     input:'public/index.html'
+  //   }
+  // },
+  // server:{
+  //   historyApiFallback:true,
+  // }
 })
